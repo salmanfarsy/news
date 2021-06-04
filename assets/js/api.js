ed5f3078f0208b8a66f6daaf0fe7db27a8b54ada
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-06-04 14:03:58 +0600

    making Nav

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,15 @@
-fetch('https://newsapi.org/v2/everything?q=bangladesh&apiKey=6b17a1a2302b424892ed9b3f63361deb')
-.then(response => response.json())
-.then(data => console.log(data));
+//news headlines 
+   fetch('https://immense-bastion-02108.herokuapp.com/api/news' ,{
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      })
+    .then(response => response.json())
+    .then(data => {
+  console.log(data.articles[0].title) 
+  const title =data.articles[0].title;
+
+    })
+
+
