08db20711b376bb7fed84be5098446d95ec5a5e8
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-06-11 14:53:34 +0600

    sources

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,6 +6,10 @@ function random(){
 const topLink = document.querySelector('#topLink');
 const topImg = document.querySelector('#topImg');
 const topNews = document.querySelector('#topNews');
+const asideLink = document.querySelectorAll('.source a');
+const asideImg= document.querySelectorAll('.source img');
+const asideTitle = document.querySelectorAll('.source p');
+console.log(asideTitle);
 //news headlines 
    fetch('https://immense-bastion-02108.herokuapp.com/api/news' ,{
         mode: 'cors',
@@ -18,11 +22,18 @@ const topNews = document.querySelector('#topNews');
 
     function topSection(data){
       const i = random();
+      const j = random();
   console.log(data.articles[i]) 
 topLink.href = data.articles[i].url;
 topImg.src = data.articles[i].urlToImage;
 topImg.alt = data.articles[i].title;
 topNews.textContent = data.articles[i].title;
+asideLink[0].href =  data.articles[j].url;
+asideImg[0].src =  data.articles[j].urlToImage;
+asideTitle[0].textContent = data.articles[j].title;
+
+
+
     }
 //source function
 function source(data){
