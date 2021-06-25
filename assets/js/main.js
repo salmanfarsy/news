b68d56d7ca3075a50c895c1c31d631579f96b3e0
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-06-25 14:46:55 +0600

    done with sticky nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 const date = document.querySelector('.date');
 const now = new Date();
 date.textContent = now.toDateString();
+//sticky nav
+const nav = document.querySelector('header');
+
+
+window.addEventListener('scroll', (e)=>{
+if(pageYOffset>nav.clientHeight){
+    nav.classList.add('sticky');
+}else{
+    nav.classList.remove('sticky'); 
+}
+})
+//menu active state
+const iNews = document.querySelector('#news');
+
+console.log(iNews.offsetTop)
