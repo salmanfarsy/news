a8a268c2363b192496002a7b230d301ab19c0557
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-06-29 21:11:20 +0600

    i think i am done

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,26 @@ const iNews = document.querySelector('#news');
 const navMenu = document.querySelector('nav');
 const navBtn = document.querySelector('.menu');
 console.log(navBtn)
-navBtn.addEventListener('click', ()=>{
+navBtn.addEventListener('click', (e)=>{
+    e.stopPropagation()
     navMenu.classList.toggle('on');
+})
+//dark mode
+const body = document.querySelector('body');
+const darkBtn = document.querySelector('#dark');
+const lightBtn = document.querySelector('#light');
+
+darkBtn.addEventListener('click', ()=>{
+    body.classList.add('dark');
+    darkBtn.classList.add('off');
+    lightBtn.classList.remove('off');
+})
+lightBtn.addEventListener('click', ()=>{
+    body.classList.remove('dark');
+    darkBtn.classList.remove('off');
+    lightBtn.classList.add('off');
+})
+//menu toggle off
+body.addEventListener('click', ()=>{
+    navMenu.classList.remove('on');
 })
